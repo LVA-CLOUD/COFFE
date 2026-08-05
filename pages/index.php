1955e8a7f1e1054005417dbad617bd80bfdf740b
StephanieRibeiro317 <steh.ribeiro317@gmail.com>
2026-08-05 09:33:01 -0300

    .
    .

--- pages/index.php
+++ pages/index.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Amantes de café</title>
+    <title>Grão & Letra</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     <link rel="stylesheet" href="../assets/css/style.css">
 </head>
@@ -54,7 +54,7 @@
             <small class="datagem-jornal">
 
             </small>
-            <h2 class="titulo-jornal"></h2>
+            <h2 class="titulo-jornal">Grão & Letra</h2>
 
             <small class="subtitulo-topo-jornal"></small>
             <hr class="linha-baixo">
